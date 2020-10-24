6c490d128856b796c2598c51961b1fcbc5c14d80
itachoco <b1615566@planet.kanazawa-it.ac.jp>
2020-10-24 17:42:10 +0900

    add : 時間経過に伴い、ジュリア集合の色が変化する機能を追加

--- main.glsl
+++ main.glsl
@@ -24,5 +24,6 @@ float culc_julia(vec2 pos, vec2 c)
 void main(void)
 {
     vec2 uv = (gl_FragCoord.xy * 2.0 - resolution.xy) / min(resolution.x, resolution.y);
-    gl_FragColor = vec4(vec3(1.0) * culc_julia(uv * 1.0, vec2(cos(time * 0.5), sin(time * 0.5))), 1.0);
+    vec3 color_vec = vec3(sin(time * 0.5) * 0.25 + 0.5, sin(time * 0.5 + 3.1415) * 0.25 + 0.5, sin(time * 0.7) * 0.25 + 0.5);
+    gl_FragColor = vec4(color_vec * culc_julia(uv * 1.0, vec2(cos(time * 0.5), sin(time * 0.5))), 1.0);
 }
